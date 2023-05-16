1f6e3f6357fc8a91be24f228cf816c5affe9c181
Amey <bamey2241997@gmail.com>
2023-05-16 00:38:34 -0700

    Enhacned by ADding configs

--- main/mail.py
+++ main/mail.py
@@ -1,28 +1,29 @@
+import sys
+import random
+from datetime import datetime as dt
 import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
-import pathlib
+
 from configs.config import *
-from configs.email_bodies import INTERN_REFERAL
+from configs.email_bodies import GRADER_MAIL, INTERN_REFERAL, SLIDING_INTO_DMS, THANK_YOU_EMAIL
 from configs.email_config import *
 from configs.dbConfig import *
-from datetime import datetime as dt
 from configs.envrinomentSpecificConfgis import TABLE_NAME
-import random
-from utils.dbUtils.dbUtils import executeCommand, executeGetCommand
+
+from utils.dbUtils.dbUtils import BASE_DIR, executeCommand, executeGetCommand
+
 class Mail:
 
     # INIT CONNECTION PARAMETERS 
     def __init__(self):
-        random_number = random.randint(1,3)
+        random_number = random.randint(1,NUMBER_OF_EMAIL_ADDRESSES)
         self.port = SMTP_PORT
         self.smtp_server_domain_name = SMTP_DOMAIN_NAME
         self.sender_mail = os.environ.get(FROM.format(random_number))
         self.password = os.environ.get(EMAIL_PASS.format(random_number))
-        self.sent_for = "Iternship Search"
-        self.position = "DATA"
 
 
     def __repr__(self) -> str:
@@ -54,74 +55,99 @@ class Mail:
 
         print(f"Record timestamp was updated in DB for: {HR.Email}")
 
-    def getInternalReferalContent(self, HR):
-        return MIMEText(INTERN_REFERAL.format(
-        firstName = HR.FirstName,
-        # aspiring_role_title = SKILL_SET[Position]["LOOKING_TITLE"],
-        companyName = HR.Company
-        # roleTitle = SKILL_SET[Position]["TITLE"],
-        # natural_interests = SKILL_SET[Position]["NATURAL_INTERESTS"],
-        # profesional_skills = SKILL_SET[Position]["SKILLS"],
-        # personalDetails=PERSONAL_DETAILS
-        ), 'html')
+    # GET EMAIL BODY FROM EMAIL_BODIES.PY
+    def getEmailBody(self, HR):
+        if FETCH_RECRUITERS:
+            return INTERN_REFERAL.format(
+                firstName = HR.FirstName,
+                userFirstName = USER_FIRST_NAME,
+                userLastName = USER_LAST_NAME,
+                applying_for = APPLYING_FOR,
+                companyName = HR.Company,
+                userCertificationURL = USER_CERTIFICATION_URL,
+                soft_skills = USER_SOFT_SKILLS,
+                userPortFolioURL = USER_PORTFOLIO_URL,
+                userGitHubURL = USER_GITHUB_URL  
+            )
+        
+        elif FETCH_DEVELOPERS:
+            return INTERN_REFERAL.format(
+                firstName = HR.FirstName,
+                userFirstName = USER_FIRST_NAME,
+                userLastName = USER_LAST_NAME,
+                applying_for = APPLYING_FOR,
+                companyName = HR.Company,
+                userCertificationURL = USER_CERTIFICATION_URL,
+                soft_skills = USER_SOFT_SKILLS,
+                userPortFolioURL = USER_PORTFOLIO_URL,
+                userGitHubURL = USER_GITHUB_URL  
+            )
+        
+        elif FETCH_TEACHERS:
+            return GRADER_MAIL
+        
+        elif SEND_FOLLOW_UP:
+             return SLIDING_INTO_DMS.format(
+                 firstName = HR.FirstName,
+                 userFirstName = USER_FIRST_NAME,
+                 userLastName = USER_LAST_NAME,
+                 userPortFolioURL = USER_PORTFOLIO_URL,
+                 companyName = HR.Company,
+             )
+        
+        elif SEND_THANK_YOU:
+             return THANK_YOU_EMAIL.format(
+                 firstName = HR.FirstName,
+                 userFirstName = USER_FIRST_NAME,
+                 userLastName = USER_LAST_NAME,
+                 companyName = HR.Company
+             )
+        else:
+            return sys.exit(1)
+    
+    # GET EMAIL BODY AS HTML
+    def getEmailAsHTML(self, HR):
+        return MIMEText(self.getEmailBody(HR), 'html')
 
-    def getTeacherMailBody(self):
-        return MIMEText(graderMail, 'html')
+    # ATTACH FILES TO MAIL SPECIFIED IN THE CONFIG FILE
+    def attachNecessaryFiles(self, mail):
+        if ATTACH_RESUME:
+             resume_path = os.path.join(BASE_DIR, ATTACHMENT_FOLDER, RESUME_FOLDER, RESUME_FILE_NAME)
+             self.attachDocument(mail, resume_path, RESUME_FILE_NAME)
+        
+        if ATTACH_COVER_LETTER:
+            cover_letter_path = os.path.join(BASE_DIR, ATTACHMENT_FOLDER, COVER_LETTER_FOLDER, COVER_LETTER_FILE_NAME)
+            self.attachDocument(mail, cover_letter_path, COVER_LETTER_FILE_NAME)
 
-    def getUniversityRelationsFollowUp(self, first_Name, company):
-        return MIMEText(followUpBody2.format(
-        firstName = first_Name,
-        companyName = company
-        ), 'html')
+        if ATTACH_MS_TRANSCRIPTS:
+            ms_transcripts_path = os.path.join(BASE_DIR, ATTACHMENT_FOLDER, TRANSCRIPTS_FOLDER, MS_TRANSCRIPTS_NAME)
+            self.attachDocument(mail, ms_transcripts_path, MS_TRANSCRIPTS_NAME)
+
+        if ATTACH_BS_TRANSCRIPTS:
+            bs_transcripts_path = os.path.join(BASE_DIR, ATTACHMENT_FOLDER, TRANSCRIPTS_FOLDER, BE_TRANSCRIPTS_NAME)
+            self.attachDocument(mail, bs_transcripts_path, BE_TRANSCRIPTS_NAME)
+
 
     def send(self, HR):
+
+        # LOGIN
         ssl_context = ssl.create_default_context()
         service = smtplib.SMTP_SSL(self.smtp_server_domain_name, self.port, context=ssl_context)
-        service.login(self.sender_mail, self.password)
-        BASE_PATH = pathlib.Path().resolve()
-
+        service.login(self.sender_mail, self.paslsword)
+        
+        # INITIATE
         mail = MIMEMultipart('alternative')
         mail['Subject'] = SUBJECT_INTERNSHIP
         mail['From'] = self.sender_mail
         mail['To'] = HR.Email
         
+        # ATTACH FILES
+        HTML_content = self.getEmailAsHTML(HR)
+        self.attachNecessaryFiles(mail)
+        mail.attach(HTML_content)
 
-        if self.position.lower() == "data":
-            text_content = self.getInternalReferalContent(HR)
-
-            # ATTACH RESUME WITH EMAIL
-            Resume_File_Path = os.path.join(RESUME_FOLDER, RESUME_FILE_NAME)
-            self.attachDocument(mail, Resume_File_Path, RESUME_FILE_NAME)
-
-        elif self.sent_for.lower() == "folloupUniv":
-            text_content = self.getUniversityRelationsFollowUp(HR.FirstName, HR.Company)
-            # print(text_content)
-
-        elif self.sent_for.lower() == "grader":
-            text_content = self.getTeacherMailBody()
-            # attach resume
-            Resume_File_Path = os.path.join(BASE_PATH, FILE_FOLDER, RESUME_FOLDER, self.osition, RESUME_FILE_NAME)
-            self.attachDocument(mail, Resume_File_Path, RESUME_FILE_NAME)
-
-            # attach transcripts
-            ms_txpts_File_Path = os.path.join(TRANSCRIPTS_FOLDER, MS_TRANSCRIPTS_NAME)
-            self.attachDocument(mail, ms_txpts_File_Path, MS_TRANSCRIPTS_NAME)
-
-            # # attach transcripts
-            # be_txpts_File_Path = os.path.join(TRANSCRIPTS_FOLDER, BE_TRANSCRIPTS_NAME)
-            # self.attachDocument(mail, be_txpts_File_Path, BETxcptsFileName)
-
-            # print(text_content)
-
-        else:
-            print("Erroring out..")
-            exit(-1)
-
-        mail.attach(text_content)
-
+        # SEND
         service.sendmail(self.sender_mail, HR.Email, mail.as_string())
         print(f"Email was sent to: {HR.Email}")
         self.makeUpdateToDB(HR)
-
-
         service.quit()
